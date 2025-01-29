03a18cb8e78b8886636f48b495c2bee27d0b587a
Oscar Ouwens <o_ouwens@hotmail.com>
2025-01-29 15:32:23 +0100

    Print to Dev tools console log with console.log()

--- cypress/e2e/Test1.js
+++ cypress/e2e/Test1.js
@@ -13,8 +13,8 @@ cy.get('.product:visible').should('have.length',4)
 cy.get('.products').as('productlocator')
 cy.get('@productlocator').find('.product').should('have.length',4)
 cy.get('@productlocator').find('.product').eq(1).contains('ADD TO CART').click()
+console.log('hoi')
 cy.get('@productlocator').find('.product').each(($el, index, $list) => {
-
 const textVeg=$el.find('h4.product-name').text()
 if(textVeg.includes('Cashews'))
 {

50bee924a7b71e190dde16c3b67367e15ee5b218
Oscar Ouwens <o_ouwens@hotmail.com>
2025-01-29 15:16:59 +0100

    Use aliasing with the as command

--- cypress/e2e/Test1.js
+++ cypress/e2e/Test1.js
@@ -10,10 +10,10 @@ cy.get('.search-keyword').type('ca')
 cy.wait(2000)
 cy.get('.product:visible').should('have.length',4)
 //Parent child chaining
-cy.get('.products').find('.product').should('have.length',4)
-cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
-
-cy.get('.products').find('.product').each(($el, index, $list) => {
+cy.get('.products').as('productlocator')
+cy.get('@productlocator').find('.product').should('have.length',4)
+cy.get('@productlocator').find('.product').eq(1).contains('ADD TO CART').click()
+cy.get('@productlocator').find('.product').each(($el, index, $list) => {
 
 const textVeg=$el.find('h4.product-name').text()
 if(textVeg.includes('Cashews'))

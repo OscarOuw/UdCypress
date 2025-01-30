67034dd2c764f4da8fd9ee6a787144e3ac1d12f4
Oscar Ouwens <o_ouwens@hotmail.com>
2025-01-30 15:41:15 +0100

    How to handle with alert/confirm

---
+++ cypress/e2e/Test4.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+
+describe('My Third Test Suite', function()
+{
+
+it('My Third Test case', function() {
+
+//Check boxes
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+cy.get('#alertbtn').click()
+cy.get('[value="Confirm"]').click()
+//window:alert
+cy.on('window:alert',(str)=>
+{
+    //Mocha
+    expect(str).to.be.equal('Hello , share this practice page and share your knowledge')
+})
+//window:confirm
+cy.on('window:confirm',(str)=>
+    {
+        //Mocha
+        expect(str).to.be.equal('Hello , Are you sure you want to confirm?')
+    })
+
+
+})
+})

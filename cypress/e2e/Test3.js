be97a7c4ccd6c84314f4d38d4f9f19a150d5a2bd
Oscar Ouwens <o_ouwens@hotmail.com>
2025-01-30 14:50:45 +0100

    Radio button check

--- cypress/e2e/Test3.js
+++ cypress/e2e/Test3.js
@@ -25,15 +25,19 @@ cy.get('.ui-menu-item div').each(($el, index, $list) => {
     }
 
 })
-
+//autocomplete
 cy.get('#autocomplete').should('have.value','India')
 
-//Show and Hide objects
+//visible invisible
 cy.get('#displayed-text').should('be.visible')
 cy.get('#hide-textbox').click()
 cy.get('#displayed-text').should('not.be.visible')
 cy.get('#show-textbox').click()
 cy.get('#displayed-text').should('be.visible')
+
+//radio button
+
+cy.get('[value="radio2"]').check().should('be.checked')
 
 })
 })

249102bee7566f0fa7dd8e6f387ffe10fdba8572
Oscar Ouwens <o_ouwens@hotmail.com>
2025-01-30 13:46:11 +0100

    Add test with static dropdown

--- cypress/e2e/Test3.js
+++ cypress/e2e/Test3.js
@@ -5,10 +5,14 @@ describe('My Third Test Suite', function()
 
 it('My Third Test case', function() {
 
+//Check boxes
 cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
 cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1' )
 cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
 cy.get('input[type="checkbox"]').check(['option2','option3'])
+
+//Static Dropdown
+cy.get('select').select('option2').should('have.value', 'option2')
 
 
 })

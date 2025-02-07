1e97f1d3d42996bf3a16cfdbe8732d62a671bb48
Oscar Ouwens <o_ouwens@hotmail.com>
2025-02-07 10:48:23 +0100

    Handling Mouse Over popups

---
+++ cypress/e2e/Test6.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+
+describe('My Fifth Test Suite', function()
+{
+
+it('My Fifth Test case', function() {
+
+//Check boxes
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+
+cy.get('.mouse-hover-content').invoke('show')
+cy.contains('Top').click()
+cy.url().should('include','top')
+})
+})
